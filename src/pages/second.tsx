bbeae8bedcf139eaa3c2f312ab7ff76d33d9c2db
Piterga12 <pitergatf@gmail.com>
2023-04-19 23:49:06 +0200

    feat: Added second page

--- src/pages/second.tsx
+++ src/pages/second.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           Portfolio de Peter González-Anta
-          <Link href={"/second"}>Next Page</Link>
+          <Link href={"/"}> Go Back</Link>
         </div>
       </main>
     </>
