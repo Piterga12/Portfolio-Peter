7ce2c06dec9c10d444064b7f46bd7e9df7c7307f
Piterga12 <pitergatf@gmail.com>
2023-04-20 00:16:55 +0200

    chore: corrected name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,8 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          Portfolio de Peter González-Anta
+          Portfolio de Peter González-Anta Torrents-Faura Rodríguez-Orta
+          Vergés-Bastardes
           <Link href={"/second"}>Next Page</Link>
         </div>
       </main>
