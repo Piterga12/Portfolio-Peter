55f03136d613df1500ee5a5eb2c7071d332a930e
Piterga12 <pitergatf@gmail.com>
2023-04-24 21:41:50 +0200

    fix: Image is in the center now

--- src/components/sliderimages.tsx
+++ src/components/sliderimages.tsx
@@ -25,7 +25,7 @@ const SliderImages: React.FC<SliderImagesProps> = ({ images }) => {
 
   return (
     <div className="relative w-full h-96">
-      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-full">
+      <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-full text-center">
         {images.map((image, index) => (
           <Image
             key={index}
@@ -33,19 +33,22 @@ const SliderImages: React.FC<SliderImagesProps> = ({ images }) => {
             alt={image.alt}
             className={`${
               index === currentSlide ? "opacity-100" : "opacity-0"
-            } transition-opacity duration-1000 ease-in-out absolute top-0 left-10`}
-            width={image.width}
-            height={image.height}
+            } transition-opacity duration-1000 ease-in-out absolute top-0 left-0 right-0 bottom-0 m-auto`}
+            layout="fill"
+            objectFit="contain"
           />
         ))}
-        <div className="absolute top-0 left-0 w-full h-full flex justify-between items-center">
-          <button className="p-2 bg-gray-800 text-white" onClick={handlePrev}>
-            Prev
-          </button>
-          <button className="p-2 bg-gray-800 text-white" onClick={handleNext}>
-            Next
-          </button>
-        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-between items-center">
+        <button
+          className="p-2 bg-gray-800 text-white mr-2"
+          onClick={handlePrev}
+        >
+          Prev
+        </button>
+        <button className="p-2 bg-gray-800 text-white" onClick={handleNext}>
+          Next
+        </button>
       </div>
     </div>
   );
