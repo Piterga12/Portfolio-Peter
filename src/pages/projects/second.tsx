cb9e37552ca2a6a8d0961bea3f5dd70b6da870ed
Piterga12 <pitergatf@gmail.com>
2023-04-27 04:09:50 +0200

    fix: dark/light mode in all menus

--- src/pages/projects/second.tsx
+++ src/pages/projects/second.tsx
@@ -3,19 +3,33 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import Link from "next/link";
 import Menu from "../../components/menu";
+import { useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [isDark, setIsDark] = useState(false);
+
+  const handleToggle = () => {
+    setIsDark(!isDark);
+  };
+  const background = isDark ? "/backgroundDark.gif" : "/backgroundLight.gif";
   return (
     <>
-      <Menu />
+      <Menu isDark={isDark} handleToggle={handleToggle} />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           Portfolio de Peter González-Anta
           <Link href={"/"}> Go Back</Link>
         </div>
       </main>
+      <style jsx>{`
+        .animate-gradient {
+          background-image: url(${background});
+          background-size: cover;
+          background-position: center;
+        }
+      `}</style>
     </>
   );
 }
