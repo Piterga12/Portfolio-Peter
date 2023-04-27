1abd06ff01fcae2584cee3d971ebe8fc4244251a
Pedro González-Anta <pitergatf@gmail.com>
2023-04-27 20:39:46 +0200

    fix: Background is a video now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
 
   const handleToggle = () => {
     setIsDark(!isDark);
+    console.log(background);
   };
   const images = [
     {
@@ -47,7 +48,7 @@ export default function Home() {
     },
   ];
 
-  const background = isDark ? "/backgroundDark.gif" : "/backgroundLight.gif";
+  const background = isDark ? "/backgroundDark.mp4" : "/backgroundLight.mp4";
 
   return (
     <>
@@ -62,40 +63,45 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Menu isDark={isDark} handleToggle={handleToggle} />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gradient-to-b from-blue-500 to-purple-500 animate-gradient">
-        Portfolio de Peter González-Anta Torrents-Faura
-        <div>
-          <TextSquare>
-            Hi, I&apos;m Pedro González Anta Torrents Faura! <br></br>
-            <br></br> I&apos;m a junior video game developer with a strong
-            background in programming languages like C++ and experience using
-            Unity to create exciting games. I&apos;m passionate about game
-            development, and my LinkedIn profile showcases my academic
-            background, experience, and skills in this field. <br></br>Through
-            my projects and internships, I have developed strong problem-solving
-            and analytical skills that are essential in game programming. I also
-            have experience working collaboratively in teams and meeting project
-            deadlines, and I&apos;m committed to delivering high-quality work.
-            In addition to my technical skills, <br></br>
-            <br></br>I&apos;m fluent in Spanish, Catalan, and English, which can
-            be a valuable asset for companies with international operations or
-            clients. <br></br>Overall, I&apos;m a motivated and skilled video
-            game developer with a strong work ethic and a commitment to
-            delivering high-quality work. I&apos;m excited to continue learning
-            and growing in my field and to create games that entertain and
-            inspire players.
-          </TextSquare>
-        </div>
-        <SliderImages images={images} />
+      <main className="flex min-h-screen flex-col items-center justify-between bg-transparent">
+        <section className="relative">
+          <video className="absolute z-[-1]" autoPlay muted loop>
+            <source src="/backgroundLight.mp4" type="video/mp4" />
+          </video>
+          <div className="p-16">
+            <TextSquare>
+              <p className="my-4 z-10">
+                {`Hi, I'm Pedro González Anta Torrents Faura!`}
+              </p>
+              <p className="my-4 z-10">
+                {`I'm a junior video game developer with a strong
+              background in programming languages like C++ and experience using
+              Unity to create exciting games. I'm passionate about game
+              development, and my LinkedIn profile showcases my academic
+              background, experience, and skills in this field.`}
+              </p>
+              <p className="my-4 z-10">
+                {`Through my projects and internships, I have developed strong problem-solving
+              and analytical skills that are essential in game programming. I also
+              have experience working collaboratively in teams and meeting project
+              deadlines, and I'm committed to delivering high-quality work.`}
+              </p>
+              <p className="my-4 z-10">
+                {`In addition to my technical skills, 
+              I'm fluent in Spanish, Catalan, and English, which can
+              be a valuable asset for companies with international operations or
+              clients. Overall, I'm a motivated and skilled video
+              game developer with a strong work ethic and a commitment to
+              delivering high-quality work. I'm excited to continue learning
+              and growing in my field and to create games that entertain and
+              inspire players.`}
+              </p>
+            </TextSquare>
+          </div>
+          <SliderImages images={images} />
+        </section>
       </main>
       <LowMenu />
-      <style jsx>{`
-        .animate-gradient {
-          background-image: url(${background});
-          background-size: cover;
-          background-position: center;
-        }
-      `}</style>
     </>
   );
 }
