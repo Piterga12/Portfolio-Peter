b82dcbc7239360467c175f4f5363bdf438ce13b1
Piterga12 <pitergatf@gmail.com>
2023-11-20 03:19:55 +0100

    chore: Tryout of waiting image

--- src/components/sliderimages2.tsx
+++ src/components/sliderimages2.tsx
@@ -2,12 +2,10 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 import {
   faChevronLeft,
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
-import "@fortawesome/fontawesome-svg-core/styles.css";
 
 interface SliderVideoProps {
   src: string;
@@ -25,6 +23,7 @@ const SliderVideos: React.FC<SliderImagesProps> = ({ videos }) => {
   const [prevSlide, setPrevSlide] = useState(videos.length - 1);
   const [nextSlide, setNextSlide] = useState(1);
   const [isMoving, setIsMoving] = useState(false);
+  const [videoLoaded, setVideoLoaded] = useState(false);
   const router = useRouter();
 
   const handleNext = () => {
@@ -37,6 +36,10 @@ const SliderVideos: React.FC<SliderImagesProps> = ({ videos }) => {
     setCurrentSlide((currentSlide + 1) % videos.length);
     setPrevSlide(currentSlide);
     setNextSlide((currentSlide + 2) % videos.length);
+  };
+
+  const handleVideoLoad = () => {
+    setVideoLoaded(true);
   };
 
   useEffect(() => {
@@ -61,24 +64,35 @@ const SliderVideos: React.FC<SliderImagesProps> = ({ videos }) => {
       <div className="absolute top-1/2 left-0 transform -translate-y-1/2 w-full h-full text-center">
         {videos.map((video, index) => (
           <div key={index}>
-            <video
-              autoPlay
-              muted
-              loop
-              className={`absolute top-0 left-0 right-0 bottom-0 m-auto object-cover ${
-                index === currentSlide
-                  ? "z-30 opacity-100 transition-all duration-1000 ease-in-out"
-                  : index === prevSlide
-                  ? "z-20 opacity-50 left-2/3 transition-all duration-1000 ease-in-out"
-                  : index === nextSlide
-                  ? "z-20 opacity-50 right-2/3 transition-all duration-1000 ease-in-out"
-                  : "z-10 opacity-0"
-              }`}
-              onTransitionEnd={handleTransitionEnd}
-              onClick={() => handleVideoClick(video.link)}
-            >
-              <source src={video.src} type={video.type} />
-            </video>
+            {videoLoaded && (
+              <video
+                autoPlay
+                muted
+                loop
+                className={`absolute top-0 left-0 right-0 bottom-0 m-auto object-cover ${
+                  index === currentSlide
+                    ? "z-30 opacity-100 transition-all duration-1000 ease-in-out"
+                    : index === prevSlide
+                    ? "z-20 opacity-50 left-2/3 transition-all duration-1000 ease-in-out"
+                    : index === nextSlide
+                    ? "z-20 opacity-50 right-2/3 transition-all duration-1000 ease-in-out"
+                    : "z-10 opacity-0"
+                }`}
+                onTransitionEnd={handleTransitionEnd}
+                onClick={() => handleVideoClick(video.link)}
+                onLoadedData={handleVideoLoad}
+              >
+                <source src={video.src} type={video.type} />
+              </video>
+            )}
+            {!videoLoaded && (
+              <Image
+                src="/images/PlayWithMe.png" // Replace with the path to your placeholder image
+                alt={video.alt}
+                width={400} // Set the width you desire
+                height={300} // Set the height you desire
+              />
+            )}
           </div>
         ))}
       </div>
