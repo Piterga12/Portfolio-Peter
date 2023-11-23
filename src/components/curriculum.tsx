7b0499e7c70abd24710c770e282b62c1a3294952
Piterga12 <pitergatf@gmail.com>
2023-11-23 03:24:32 +0100

    chore: Responsive cv

--- src/components/curriculum.tsx
+++ src/components/curriculum.tsx
@@ -1,7 +1,5 @@
+import { useEffect, useState } from "react";
 import { Inter } from "next/font/google";
-import Menu from "./menu";
-import LowMenu from "./lowmenu";
-import Meta from "./meta";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,19 +7,45 @@ interface PDFViewerProps {
   pdfUrl: string;
 }
 
-const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl }) => (
-  <>
+const PDFViewer: React.FC<PDFViewerProps> = ({ pdfUrl }) => {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+
+    handleResize(); // Initial check
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return (
     <div className="flex flex-col items-center justify-between overflow-hidden">
       <h2 className="mt-16 font-oswald text-slate-900 underline text-4xl z-10">
         Curriculum
       </h2>
-      <embed
-        className="my-8 h-screen w-full sm:w-2/3"
-        src={`/documents/${pdfUrl}#zoom=25`}
-        type="application/pdf"
-      />
+      {isSmallScreen ? (
+        <a
+          href={`/documents/${pdfUrl}`}
+          download
+          className="my-8 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 cursor-pointer transition-colors duration-300"
+        >
+          Download Curriculum (PDF)
+        </a>
+      ) : (
+        <embed
+          className="my-8 h-screen w-full sm:w-2/3"
+          src={`/documents/${pdfUrl}#zoom=25`}
+          type="application/pdf"
+        />
+      )}
     </div>
-  </>
-);
+  );
+};
 
 export default PDFViewer;
